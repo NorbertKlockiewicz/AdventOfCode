480957cd92b5162fa6af8e74cc3428c8979627af
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2023-12-10 13:25:12 +0100

    day 01

---
+++ 2023/day01/solution.py
@@ -0,0 +1,85 @@
+import math
+
+class Solution:
+    def __init__(self):
+        self.data = []
+
+    def load_data(self, file_name: str):
+        with open(file_name) as f:
+            self.data = f.read().splitlines()
+
+    def solve_part1(self):
+        sum = 0
+        for line in self.data:
+            first_digit = 0
+            last_digit = 0
+            for letter in line:
+                try:
+                    first_digit = int(letter)
+                    break
+                except ValueError:
+                    pass
+
+            for letter in line[::-1]:
+                try:
+                    last_digit = int(letter)
+                    break
+                except ValueError:
+                    pass
+
+            sum += first_digit * 10 + last_digit
+
+        print("Part 1 Answer:", sum)
+
+    def solve_part2(self):
+        dict = {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+        }
+
+        sum = 0
+        for line in self.data:
+            first_digit_index = math.inf
+            last_digit_index = 0
+            first_digit = 0
+            last_digit = 0
+            for key in dict:
+                if key in line:
+                    if line.index(key) < first_digit_index:
+                        first_digit_index = line.index(key)
+                        first_digit = dict[key]
+
+                    reversed_line = line[::-1]
+
+                    if key[::-1] in reversed_line:
+                        if len(line) - reversed_line.index(key[::-1]) - 1 > last_digit_index:
+                            last_digit_index = len(line) - reversed_line.index(key[::-1]) - 1
+                            last_digit = dict[key]
+
+            for letter in line:
+                try:
+                    if line.index(letter) < first_digit_index:
+                        first_digit = int(letter)
+                        first_digit_index = line.index(letter)
+                        break
+                except ValueError:
+                    pass
+
+            for i, letter in enumerate(line[::-1]):
+                try:
+                    if len(line) - i > last_digit_index:
+                        last_digit = int(letter)
+                        break
+                except ValueError:
+                    pass
+
+            sum += first_digit * 10 + last_digit
+
+        print("Part 2 Answer:", sum)
--- 2023/main.py
+++ 2023/main.py
@@ -1,2 +1,7 @@
+from day01 import solution as s1
 
-
+if __name__ == '__main__':
+    solution = s1.Solution()
+    solution.load_data('day01/input.txt')
+    solution.solve_part1()
+    solution.solve_part2()
